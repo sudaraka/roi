266aae3c07e4b902920517979b2ce9f0d50ac548
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-11-13 02:04:53 +0530

    Feat: [ui] render base columns of the table

--- src/index.js
+++ src/index.js
@@ -12,11 +12,50 @@
 
 import xs from 'xstream'
 import { run } from '@cycle/xstream-run'
-import { section, makeDOMDriver } from '@cycle/dom'
+import { div, section, makeDOMDriver } from '@cycle/dom'
 
 const
   drivers = { 'DOM': makeDOMDriver('#app') },
 
-  main = () => ({ 'DOM': xs.of(section('Column 1')) })
+  identity = _ => _,
+
+  component = (intent = identity) => (model = identity) => view => source => ({
+    ...source,
+    'DOM': view(model(intent(source)))
+  }),
+
+  column = source => {
+    const
+      intent = src => src.props,
+      view = state$ => state$.map(state => section([ div(state.title) ]))
+
+    return component(intent)()(view)(source)
+  },
+
+  hscroll = source => {
+    const
+      intent = src => src.props.columns,
+
+      view = state$ => state$
+        .map(title => column({ 'props': xs.of({ title }) }).DOM)
+        .flatten()
+        .fold((acc, col$) => [ ...acc, col$ ], [])
+        .last()
+        .map(columns => div('.hscroll', columns))
+
+    return component(intent)()(view)(source)
+  },
+
+  main = () => {
+    const
+      titleColumn$ = column({ 'props': xs.of({ 'title': 'xxx' }) }).DOM,
+      hscroll$ = hscroll({ 'props': { 'columns': xs.of('one', 'two', 'three', 'four', 'five') } }).DOM,
+      totalColumn$ = column({ 'props': xs.of({ 'title': 'yyy' }) }).DOM
+
+    return {
+      'DOM': xs.combine(titleColumn$, hscroll$, totalColumn$)
+        .map(col => div('.container', col))
+    }
+  }
 
 run(main, drivers)
