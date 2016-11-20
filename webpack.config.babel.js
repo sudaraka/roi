44e52b84e90be47e56b52d780ef0f0ae5e4cecf0
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-11-20 22:53:08 +0530

    Dev: split develpomet/production configuration

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -14,68 +14,80 @@ import { resolve } from 'path'
 import ExtractText from 'extract-text-webpack-plugin'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import { IgnorePlugin, NamedModulesPlugin } from 'webpack'
+import merge from 'webpack-merge'
 
-export default env => ({
-  'context': resolve('src'),
+export default env => {
+  const
+    baseConfig = {
+      'context': resolve('src'),
 
-  'entry': {
-    'index.js': [
-      './index.js',
-      './main.sass'
-    ]
-  },
-
-  'output': {
-    'path': resolve('dist'),
-    'publicPath': '/',
-    'filename': '[name]'
-  },
-
-  'module': {
-    'loaders': [
-      {
-        'test': /\.js$/,
-        'exclude': /node_modules/,
-        'loaders': [ 'babel-loader' ]
+      'entry': {
+        'index.js': [
+          './index.js',
+          './main.sass'
+        ]
       },
-      {
-        'test': /\.sass$/,
-        'exclude': /node_modules/,
-        'loaders': ExtractText.extract([ 'css-loader', 'sass-loader' ])
+
+      'output': {
+        'path': resolve('dist'),
+        'publicPath': '/',
+        'filename': '[name]'
+      },
+
+      'module': {
+        'loaders': [
+          {
+            'test': /\.js$/,
+            'exclude': /node_modules/,
+            'loaders': [ 'babel-loader' ]
+          },
+          {
+            'test': /\.sass$/,
+            'exclude': /node_modules/,
+            'loaders': ExtractText.extract([ 'css-loader', 'sass-loader' ])
+          }
+        ]
+      },
+
+      'plugins': [
+        // Exclude moment.js locale from the build
+        new IgnorePlugin(/locale/, /moment$/),
+
+        new HtmlWebpackPlugin({
+          'filename': 'index.html',
+          'template': './index.html',
+          'minify': {
+            'collapseBooleanAttributes': true,
+            'collapseInlineTagWhitespace': true,
+            'collapseWhitespace': true,
+            'decodeEntities': true,
+            'removeAttributeQuotes': true,
+            'removeComments': true,
+            'removeEmptyAttributes': true,
+            'removeRedundantAttributes': true,
+            'removeScriptTypeAttributes': true,
+            'removeStyleLinkTypeAttributes': true,
+            'useShortDoctype': true
+          }
+        }),
+        new ExtractText('styles.css')
+      ]
+    },
+
+    productionConfig = {},
+
+    developmentConfig = {
+      'plugins': [
+        new NamedModulesPlugin()
+      ],
+
+      'devtool': 'eval',
+
+      'devServer': {
+        'host': '127.0.0.1',
+        'port': 5000
       }
-    ]
-  },
+    }
 
-  'plugins': [
-    // Exclude moment.js locale from the build
-    new IgnorePlugin(/locale/, /moment$/),
-
-    new HtmlWebpackPlugin({
-      'filename': 'index.html',
-      'template': './index.html',
-      'minify': {
-        'collapseBooleanAttributes': true,
-        'collapseInlineTagWhitespace': true,
-        'collapseWhitespace': true,
-        'decodeEntities': true,
-        'removeAttributeQuotes': true,
-        'removeComments': true,
-        'removeEmptyAttributes': true,
-        'removeRedundantAttributes': true,
-        'removeScriptTypeAttributes': true,
-        'removeStyleLinkTypeAttributes': true,
-        'useShortDoctype': true
-      }
-    }),
-    new ExtractText('styles.css'),
-
-    new NamedModulesPlugin()
-  ],
-
-  'devtool': env.prod ? '' : 'eval',
-
-  'devServer': {
-    'host': '127.0.0.1',
-    'port': 5000
-  }
-})
+  return merge(baseConfig, env.prod ? productionConfig : developmentConfig)
+}
