dce6dfc8634491d7fdc81b032022f25fc76c430b
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-12-05 15:31:02 +0530

    Feat: [function] add function to calculate matuarity dates & amounts

--- src/helper.js
+++ src/helper.js
@@ -10,15 +10,52 @@
  *
  */
 
+import moment from 'moment'
+
 const
   numberFormat = num => (parseFloat(num) || '').toLocaleString('en', {
     'minimumFractionDigits': 2,
     'maximumFractionDigits': 2
   }),
 
+  _nextMatuatiry = ({ amount, interestRate, investedDate, period }) => {
+    const
+      roi = amount * interestRate / 100 / 365 * period,
+      date = moment(investedDate).add(period, 'days'),
+      next = {
+        amount,
+        interestRate,
+        period,
+        'investedDate': date
+      }
+
+    if(date.isAfter(moment().add(1, 'year'))) {
+      // Resulting date is more than 1 year after today
+      // Terminal condition!
+
+      return {}
+    }
+
+    if(date.isBefore(moment())) {
+      // Resulting date has passed
+      // Continue calculation without taking this occurrence in to account
+
+      return { ..._nextMatuatiry(next) }
+    }
+
+    return {
+      [`${date.month()}`]: {
+        roi,
+        date
+      },
+      ..._nextMatuatiry(next)
+    }
+  },
+
   calculateReturns = account => ({
     ...account,
-    'monthlyRevenue': account.amount * account.interestRate / 100 / 12
+    'monthlyRevenue': account.amount * account.interestRate / 100 / 12,
+    'matuarities': _nextMatuatiry(account)
   })
 
 export { numberFormat, calculateReturns }
