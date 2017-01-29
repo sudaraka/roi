91393e4939ad7fc7c3bf34ce6e64cb2b060a6fd3
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2017-01-29 13:54:24 +0530

    Feat: [core] add skeleton Preact application

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+/**
+ * src/components/App/App.js: main application component
+ *
+ * Copyright 2017 Sudaraka Wijesinghe <sudaraka@sudaraka.org>
+ *
+ * This program comes with ABSOLUTELY NO WARRANTY;
+ * This is free software, and you are welcome to redistribute it and/or modify
+ * it under the terms of the BSD 2-clause License. See the LICENSE file for more
+ * details.
+ *
+ */
+
+import { h } from 'preact'
+
+export default () => <div>Accounts table goes here</div>
---
+++ src/index.js
@@ -0,0 +1,30 @@
+/**
+ * src/index.js: main application entry point
+ *
+ * Copyright 2016, 2017 Sudaraka Wijesinghe <sudaraka@sudaraka.org>
+ *
+ * This program comes with ABSOLUTELY NO WARRANTY;
+ * This is free software, and you are welcome to redistribute it and/or modify
+ * it under the terms of the BSD 2-clause License. See the LICENSE file for more
+ * details.
+ *
+ */
+
+import { h, render } from 'preact'
+
+let
+  rootElement
+
+const
+  init = () => {
+    const
+      { 'default': App } = require('Component/App')  // eslint-disable-line global-require
+
+    rootElement = render(<App />, document.querySelector('#app'), rootElement)
+  }
+
+init()
+
+if(module.hot) {
+  module.hot.accept('Component/App', init)
+}
