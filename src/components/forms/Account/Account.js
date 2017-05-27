4d01e2692dc42b6d6e9911465c7741058d4f9b84
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2017-05-27 11:54:17 +0530

    Feat: [ui] render form using existing account data

--- src/components/forms/Account/Account.js
+++ src/components/forms/Account/Account.js
@@ -13,6 +13,7 @@
 import { h } from 'preact'
 import { connect } from 'preact-redux'
 import { route } from 'preact-router'
+import moment from 'moment'
 
 import Box from 'Component/Box'
 
@@ -23,59 +24,73 @@ const
     route('/', true)
   },
 
-  Account = ({ typeList }) => (
-    <Box>
-      <form className='account-form'>
-        <div className='form-block'>
-          <label>Account Type</label>
-          <select className='form-control'>
-            { typeList.map(value => <option key={ value }>{ value }</option>) }
-          </select>
-        </div>
+  Account = ({ typeList, account }) => {
+    const
+      { number, type, amount, investedDate, interestRate, period } = account || {}
 
-        <div className='form-block'>
-          <label>Account Number</label>
-          <input className='form-control' type='text' />
-        </div>
-
-        <div className='form-block'>
-          <label>Invested Amount</label>
-          <input className='form-control' type='number' min='0' />
-        </div>
-
-        <div className='form-block'>
-          <label>Invested Date</label>
-          <input className='form-control' type='date' />
-        </div>
-
-        <div className='form-block'>
-          <label>Intrest Rate</label>
-          <input className='form-control' type='number' max='100' min='0' />
-        </div>
-
-        <div className='form-block range-block'>
-          <label>Matuarity Period</label>
-          <div className='group'>
-            <span>{ 91 } days</span>
-            <input className='form-control' type='range' max='365' min='30' />
+    return (
+      <Box>
+        <form className='account-form'>
+          <div className='form-block'>
+            <label>Account Type</label>
+            <select className='form-control'>
+              { typeList.map(value => (
+                <option key={ value } selected={ value === type }>{ value }</option>
+              )) }
+            </select>
           </div>
-        </div>
 
-        <div className='button-block'>
-          <button
-            className='btn btn-secondary'
-            type='button'
-            onClick={ handleCloseClick }
-          >Cancel</button>
+          <div className='form-block'>
+            <label>Account Number</label>
+            <input className='form-control' type='text' value={ number } />
+          </div>
 
-          <button
-            className='btn btn-success'
-            type='button'
-          >Save</button>
-        </div>
-      </form>
-    </Box>
-  ),
+          <div className='form-block'>
+            <label>Invested Amount</label>
+            <input className='form-control' type='number' min='0' step='0.01' value={ amount } />
+          </div>
+
+          <div className='form-block'>
+            <label>Invested Date</label>
+            <input className='form-control' type='date' value={ moment(investedDate).format('YYYY-MM-DD') } />
+          </div>
+
+          <div className='form-block'>
+            <label>Intrest Rate</label>
+            <input
+              className='form-control'
+              type='number'
+              max='100'
+              min='0'
+              step='0.05'
+              value={ interestRate }
+              />
+          </div>
+
+          <div className='form-block range-block'>
+            <label>Matuarity Period</label>
+            <div className='group'>
+              <span>{ period } days</span>
+              <input className='form-control' type='range' max='365' min='30' value={ period } />
+            </div>
+          </div>
+
+          <div className='button-block'>
+            <button
+              className='btn btn-secondary'
+              type='button'
+              onClick={ handleCloseClick }
+            >Cancel</button>
+
+            <button
+              className='btn btn-success'
+              type='button'
+            >Save</button>
+          </div>
+        </form>
+      </Box>
+    )
+  },
 
   state2Props = state => ({ ...state.forms })
 
