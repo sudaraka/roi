4b75fe09698472b17b5f3421dd560a0adec2a938
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2017-07-02 13:50:56 +0530

    Fix: [ui] sort account list before rendering
    This prevet the account column from jumping around after changes are saved.

--- src/components/HScroll/HScroll.js
+++ src/components/HScroll/HScroll.js
@@ -17,7 +17,39 @@ import Account from 'Column/Account'
 
 export default ({ accounts }) => {
   const
-    columns = accounts.map(acc => <Account key={ acc.number } { ...acc } />)
+    typeOrder = type => ({
+      'T-Bill': 1,
+      'FD': 2,
+      'Saving': 3
+    }[type] || 9999),
+
+    accountDisplayOrder = (a, b) => {
+      const
+        { 'type': typeA, 'number': numA } = a,
+        { 'type': typeB, 'number': numB } = b
+
+      if(typeOrder(typeA) < typeOrder(typeB)) {
+        return -1
+      }
+      else if(typeOrder(typeA) > typeOrder(typeB)) {
+        return 1
+      }
+
+      // Both account have the same type, continue to sort by account number
+
+      if(numA < numB) {
+        return -1
+      }
+      else if(numA > numB) {
+        return 1
+      }
+
+      return 0
+    },
+
+    columns = [ ...accounts ]
+      .sort(accountDisplayOrder)
+      .map(acc => <Account key={ acc.number } { ...acc } />)
 
   return (
     <div className='hscroll'>
